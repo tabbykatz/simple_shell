219410e65ed8cdc7c50c99c9d95d192464b75387
tabbykatz <tomelay@gmail.com>
2020-04-15 18:12:42 +0000

    make betty compliant

--- helpers2.c
+++ helpers2.c
@@ -67,8 +67,8 @@ char *_strchr(char *s, char c)
 	return (NULL);
 }
 /**
- *   * do_nothing - a dummy function
- *     * @nothing: a dummy integer
- *       */
+ * do_nothing - a dummy function
+ * @nothing: a dummy integer
+ */
 void do_nothing(__attribute__((unused))int nothing)
 {}
